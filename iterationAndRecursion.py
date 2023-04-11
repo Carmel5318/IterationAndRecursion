e9e9e2b445299aca077d508cbcff129fca6239e4
username <useremail>
2023-04-11 09:32:14 -0700

    Add project files.

---
+++ iterationAndRecursion.py
@@ -0,0 +1,17 @@
+def factorial_recursive(num):
+    if num == 0 or num ==1:
+        return 1 
+    else: 
+        return num * factorial_recursive(num-1)
+
+def factorial_iterative(num): 
+    fact = 1 
+    for number in range (2, num=1):
+
+
+
+
+
+
+
+if __name__"__main__":
