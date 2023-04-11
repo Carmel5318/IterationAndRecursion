312af6f1b08d25f1d970ebb469788c878dac5f97
username <useremail>
2023-04-11 09:35:54 -0700

    update

--- iterationAndRecursion.py
+++ iterationAndRecursion.py
@@ -7,6 +7,11 @@ def factorial_recursive(num):
 def factorial_iterative(num): 
     fact = 1 
     for number in range (2, num=1):
+        fact = number * fact
+    return fact
+
+def main ():
+    numlist = [0, 5, 10, 25, 100]
 
 
 
